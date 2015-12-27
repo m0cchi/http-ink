2ea2677610293386c56161816f00c8c080d395bb
mocchi <boom.boom.planet@gmail.com>
2015-12-27 17:30:54 +0900

    modify indent

--- src/response-util.lisp
+++ src/response-util.lisp
@@ -34,10 +34,9 @@
                       :connection (connection env)
                       :content-type content-type)
         :body body))
-                      
 
 (defun response-with-file (env file-path)
   (let ((file (read-file file-path))
         (content-type (format nil "~A~:[~;~:*; charset=~A~]"
-                                              (trivial-mimes:mime file-path) "utf-8")))
+                              (trivial-mimes:mime file-path) "utf-8")))
     (response env "200 OK" content-type file)))
