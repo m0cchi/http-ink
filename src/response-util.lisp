29952f510a08937a10ab5a874572b154ba38bfc2
mocchi <boom.boom.planet@gmail.com>
2016-01-03 16:56:15 +0900

    fix response-util args

--- src/response-util.lisp
+++ src/response-util.lisp
@@ -30,23 +30,29 @@
                     (read-sequence buf stream)
                     buf)))
 
-(defun respond (env status content-type body)
-  (list :header (list "HTTP/1.1" status
-                      :date (format-rfc1123-timestring nil 
-                                                       (universal-to-timestamp (get-universal-time)))
-                      :server +SERVER_NAME+
-                      :connection (connection env)
-                      :content-type content-type)
-        :body body))
+(defun respond (env status content-type body &key (header '()))
+  `(:header ,(remove nil `("HTTP/1.1" ,status
+                           :date ,(format-rfc1123-timestring nil 
+                                                             (universal-to-timestamp (get-universal-time)))
+                           :server ,+SERVER_NAME+
+                           :connection ,(connection env)
+                           :content-type ,content-type
+                           ,@header))
+            :body ,body))
 
-(defun respond-with-html (env text &key (charset "utf-8") (status "200 OK"))
+(defun respond-with-html (env text &key (charset "utf-8") (status "200 OK") (header '()))
   (respond env
            status
            (format nil "text/html ~a" charset)
-           text))
+           text
+           :header header))
 
-(defun respond-with-file (env file-path &key (status "200 OK"))
+(defun respond-with-file (env file-path &key (status "200 OK") (header '()))
   (let ((file (read-file file-path))
         (content-type (format nil "~A~:[~;~:*; charset=~A~]"
                               (mime file-path) "utf-8")))
-    (respond env status content-type file)))
+    (respond env
+             status
+             content-type
+             file
+             :header header)))
